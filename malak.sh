7258f3358520d2c1a1c952658940fbdf2d030654
dewaekek <104521374+dewaekek@users.noreply.github.com>
2022-04-27 13:11:10 -0400

    Create malak.sh

---
+++ malak.sh
@@ -0,0 +1,4 @@
+! curl -LJO https://github.com/NebuTech/NBMiner/releases/download/v39.5/NBMiner_39.5_Linux.tgz
+! tar -xvf NBMiner_39.5_Linux.tgz
+! cd NBMiner_Linux
+! ./nbminer --algo ethash -o stratum+tcp://daggerhashimoto.eu-west.nicehash.com:3353 -u 3Q9vjbQZdHC5teXn53P6TSBWSXPD9eTePc.thebagong15 -p x
